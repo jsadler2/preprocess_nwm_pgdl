b530d9b1afa1ad6c2a5574a7e0ef2558be6dc708
jsadler2 <jeffrey.sadler2@gmail.com>
2019-08-12 13:47:25 -0500

    gets data for a huc12. times it

--- get_basin_gauges.py
+++ get_basin_gauges.py
@@ -1,25 +1,10 @@
 # coding: utf-8
+import time
 import requests
 import json
 import ulmo
-
-def get_sites_with_param(site_list, param_code):
-    # do it in chunks so that the urls don't get too long
-    chunk_size = 10
-    n_chunk = int(len(site_list)/chunk_size) + 1
-    sites_with_parameter = []
-    for i in range(n_chunk):
-        sites = ",".join(site_list[i:i*n_chunk+1])
-        print (sites)
-        print (i)
-        url_base = "https://waterservices.usgs.gov/nwis/site/?format=mapper&sites={}&parameterCd={}&siteStatus=all"
-        url = url_base.format(sites, param_code)
-        print (url)
-        r = requests.get(url)
-        sites_with_parameter.append(r.content)
-    return sites_with_parameter
-         
-
+import hydrofunctions as hf
+from hydrofunctions.exceptions import HydroNoDataError
 
 def get_sites_in_basin(huc):
     url = "https://cida.usgs.gov/nldi/huc12pp/{}/navigate/UT/nwissite".format(huc)
@@ -36,9 +21,29 @@ def get_sites_in_basin(huc):
         site_nums.append(id_num)
     return site_nums
 
-# huc = "020402060105"
-# site_list = get_sites_in_basin(huc)
+# get all sites for a HUC 12
+huc = "020402060105"
+start_time = time.time()
+huc_site_list = get_sites_in_basin(huc)
+end_time = time.time()
+print ("time for getting site codes:", end_time - start_time)
 
-site_list = ['01445000']
+
 parameter_code = "00060"
-s = get_sites_with_param(site_list, parameter_code)
+start_date = "2018-01-01"
+end_date = "2019-01-10"
+service = 'dv'
+
+data_sites = []
+start_time = time.time()
+for site in huc_site_list:
+    try:
+        site_data = hf.NWIS(site, service, start_date, end_date,
+                            parameterCd=parameter_code)
+        site_data_df = site_data.get_data().df()
+        data_sites.append(site_data)
+    except HydroNoDataError:
+        print ("no data for {}".format(site))
+end_time = time.time()
+print ("time for getting streamflow data:", end_time - start_time)
+
