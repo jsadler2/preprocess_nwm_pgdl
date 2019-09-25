7512600692d2f24a05c13e45a4794ce84e6ccf84
Sadler <jsadler@usgs.gov>
2019-09-25 11:37:50 -0500

    breaking out more methods, non-approved screening

--- get_all_streamflow_data.py
+++ get_all_streamflow_data.py
@@ -12,7 +12,14 @@ def get_sites_for_huc2(huc2):
     sites_for_huc2 = df_for_huc2['code']
     return sites_for_huc2.to_list()
 
+
 def get_all_streamflow_data_for_huc2(huc2, num_chunks=10, start_date_all="1900-01-01", end_date_all='2019-09-05'):
+    """
+    gets all streamflow data for all time for a given huc2. All of the stations
+    in the HUC2 are taken at once, but the calls are chunked by date
+
+    TODO: chunk by station too
+    """
     date_chunks = pd.date_range(start_date_all, end_date_all, periods=num_chunks)
     huc_combined = []
     for i in range(len(date_chunks) - 1):
@@ -28,11 +35,14 @@ def get_all_streamflow_data_for_huc2(huc2, num_chunks=10, start_date_all="1900-0
 def get_streamflow_data(huc2, start_date, end_date):
     response = call_nwis_service(huc2, start_date, end_date)
     data = json.loads(response.text)
-    streamflow_df = nwis_json_to_df(data)
+    streamflow_df = nwis_json_to_df(data, start_date, end_date)
     return streamflow_df
 
 
 def call_nwis_service(huc2, start_date, end_date):
+    """
+    gets the data for all sites in a huc2 from a start data to an end date
+    """
     baseurl = "http://waterservices.usgs.gov/nwis/iv/?format=json&huc={}&startDT={}&endDT={}&parameterCd=00060&siteStatus=all"
     url = baseurl.format(huc2, start_date, end_date)
     request_start_time = datetime.datetime.now()
@@ -44,7 +54,88 @@ def call_nwis_service(huc2, start_date, end_date):
     return r
 
 
-def nwis_json_to_df(json_data):
+def format_dates(datetime_txt):
+    # convert datetime
+    datetime_ser = pd.to_datetime(datetime_txt, utc=True)
+    # remove  the time zone info since we are now in utc
+    datetime_ser = datetime_ser.dt.tz_localize(None)
+    return datetime_ser
+
+
+def resample_reindex(df, start_date, end_date, time_scale):
+    # resample to get mean at correct time scale 
+    df_resamp = df.resample(time_scale).mean()
+
+    # get new index
+    date_index = pd.date_range(start=start_date, end=end_date,
+                                  freq=time_scale)
+    # make so the index goes from start to end regardless of actual data
+    # presence
+    df_reindexed = df_resamp.reindex(date_index)
+    return df_reindexed
+
+
+def delete_non_approved_data(df):
+    """
+    disregard the data that do not have the "approved" tag in the qualifier
+    column
+    :param df: dataframe with qualifiers
+    :return: dataframe with just the values that are approved
+    """
+    # first I have to get the actual qualifiers. originally, these are lists
+    # in a column in the df (e.g., [A, [91]]
+    # todo: what does the number mean (i.e., [91])
+    qualifier_df = pd.DataFrame(df['qualifiers'].to_list(),
+                                columns=['qualifier_code', 'number'],
+                                index=df.index)
+    approved_indices = (qualifier_df['qualifier_code'] == 'A')
+    approved_df = df[approved_indices]
+    return approved_df
+
+
+def format_df(ts_df, site_code, start_date, end_date, time_scale,
+              only_approved=True):
+    """
+    format unformatted dataframe. this includes setting a datetime index,
+    resampling, reindexing to the start and end date,
+    renaming the column to the site code, removing the qualifier column and
+    optionally screening out any data points that are not approved
+    :param ts_df: (dataframe) unformatted time series dataframe from nwis json
+    data
+    :param site_code: (str) the site_code of the site (taken from json data)
+    :param start_date: (str) start date of call
+    :param end_date: (str) end date of call
+    :param time_scale: (str) time scale in which you want to resample and at
+    which your new index will be. should be a code (i.e., 'H' for hourly)
+    :param only_approved: (bool) whether or not to screen out non-approved data
+    points
+    :return: formatted dataframe
+    """
+    # convert datetime
+    ts_df['dateTime'] = format_dates(ts_df['dateTime'])
+    ts_df.set_index('dateTime', inplace=True)
+
+    # get rid of any points that were not approved
+    ts_df = delete_non_approved_data(ts_df)
+    # delete qualifiers column 
+    del ts_df['qualifiers']
+    # rename the column from 'value' to the site_code
+    ts_df = ts_df.rename(columns={'value': site_code})
+    # make the values numeric
+    ts_df[site_code] = pd.to_numeric(ts_df[site_code])
+
+    ts_df = resample_reindex(ts_df, start_date, end_date, time_scale)
+
+    return ts_df
+
+
+def nwis_json_to_df(json_data, start_date, end_date, time_scale='H'):
+    """
+    combine time series in json produced by nwis web from multiple sites into
+    one pandas df. the df is also resampled to a time scale and reindexed so
+    the dataframes are from the start date to the end date regardless of
+    whether there is data available or not
+    """
     df_collection = []
     time_series = json_data['value']['timeSeries']
     for ts in time_series:
@@ -54,14 +145,8 @@ def nwis_json_to_df(json_data):
         ts_data = ts['values'][0]['value']
         if ts_data:
             ts_df = pd.DataFrame(ts_data)
-            ts_df.index = pd.to_datetime(ts_df['dateTime'], utc=True)
-            del ts_df['dateTime']
-            del ts_df['qualifiers']
-            # rename the column from 'value' to the site_code
-            ts_df = ts_df.rename(columns={'value': site_code})
-            ts_df[site_code] = pd.to_numeric(ts_df[site_code])
-            ts_df = ts_df.resample('15T').mean()
-            df_collection.append(ts_df)
+            ts_df_formatted = format_df(ts_df, site_code, start_date, end_date, time_scale)
+            df_collection.append(ts_df_formatted)
     if df_collection:
         return pd.concat(df_collection)
     else:
