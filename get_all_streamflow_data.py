1e1e0a8c8894454255d7de859fbe9e2e410f5c23
Sadler <jsadler@usgs.gov>
2019-09-25 14:27:01 -0500

    chunks by site, appends to zarr, smart restart

--- get_all_streamflow_data.py
+++ get_all_streamflow_data.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import xarray as xr
 import numpy as np
@@ -7,50 +8,85 @@ import datetime
 
 
 def get_sites_for_huc2(huc2):
-    df = pd.read_csv('all_streamflow_sites_CONUS.csv', dtype={'huc': str, 'code':str})
+    df = pd.read_csv('all_streamflow_sites_CONUS.csv',
+                     dtype={'huc': str, 'code': str})
     df_for_huc2 = df[df['huc'].str.startswith(huc2)]
     sites_for_huc2 = df_for_huc2['code']
     return sites_for_huc2.to_list()
 
 
-def get_all_streamflow_data_for_huc2(huc2, num_chunks=10, start_date_all="1900-01-01", end_date_all='2019-09-05'):
+def divide_chunks(l, n): 
+    # looping till length l 
+    for i in range(0, len(l), n):  
+        yield l[i:i + n] 
+  
+
+def get_all_streamflow_data_for_huc2(huc2, output_zarr, num_sites_per_chunk=5,
+                                     start_date_all="1970-01-01",
+                                     end_date_all='2019-01-01'):
     """
-    gets all streamflow data for all time for a given huc2. All of the stations
-    in the HUC2 are taken at once, but the calls are chunked by date
-
-    TODO: chunk by station too
+    gets all streamflow data for a date range for a given huc2. Calls are 
+    chunked by station
     """
-    date_chunks = pd.date_range(start_date_all, end_date_all, periods=num_chunks)
-    huc_combined = []
-    for i in range(len(date_chunks) - 1):
-        start_date = date_chunks[i].date()
-        end_date = date_chunks[i + 1].date()
-        streamflow_data_chunk = get_streamflow_data(huc2, start_date, end_date)
-        huc_combined.append(streamflow_data_chunk)
-    all_huc_df = pd.concat(huc_combined, axis=0)
-    all_huc_df = all_huc_df.replace(-999999, np.nan)
-    return all_huc_df
+    site_codes_in_huc2 = get_sites_for_huc2(huc2)
+    start_index = get_last_site_code_position(output_zarr, site_codes_in_huc2)
+    site_codes_chunked = divide_chunks(site_codes_in_huc2[start_index:],
+                                       num_sites_per_chunk)
+
+    # loop through site_code_chunks
+    for site_chunk in site_codes_chunked:
+        streamflow_df_sites = get_streamflow_data(site_chunk,
+                                                  start_date_all,
+                                                  end_date_all)
+        append_to_zarr(streamflow_df_sites, output_zarr)
 
 
-def get_streamflow_data(huc2, start_date, end_date):
-    response = call_nwis_service(huc2, start_date, end_date)
+def get_last_site_code_position(output_zarr, all_site_codes):
+    # check if zarr dataset exists
+    if os.path.exists(output_zarr):
+        # read zarr
+        ds = xr.open_zarr(output_zarr)
+        site_codes = ds['site_code']
+        last_site_code = str(site_codes[-1].values)
+        return all_site_codes.index(last_site_code) + 1
+    else:
+        return 0
+
+
+def convert_df_to_dataset(streamflow_df):
+    data_array = xr.DataArray(streamflow_df.values,
+                              [('datetime', streamflow_df.index),
+                               ('site_code', streamflow_df.columns)])
+    data_set = xr.Dataset({'streamflow': data_array})
+    return data_set
+
+
+def append_to_zarr(streamflow_df, output_zarr):
+    ds = convert_df_to_dataset(streamflow_df)
+    ds.to_zarr(output_zarr, append_dim='site_code', mode='a')
+
+
+def get_streamflow_data(sites, start_date, end_date):
+    response = call_nwis_service(sites, start_date, end_date)
     data = json.loads(response.text)
     streamflow_df = nwis_json_to_df(data, start_date, end_date)
     return streamflow_df
 
 
-def call_nwis_service(huc2, start_date, end_date):
+def call_nwis_service(sites, start_date, end_date):
     """
-    gets the data for all sites in a huc2 from a start data to an end date
+    gets the data for a list of sites from a start date to an end date
     """
-    baseurl = "http://waterservices.usgs.gov/nwis/iv/?format=json&huc={}&startDT={}&endDT={}&parameterCd=00060&siteStatus=all"
-    url = baseurl.format(huc2, start_date, end_date)
+    base_url = "http://waterservices.usgs.gov/nwis/iv/?format=json&sites={}&" \
+               "startDT={}&endDT={}&parameterCd=00060&siteStatus=all"
+    url = base_url.format(",".join(sites), start_date, end_date)
     request_start_time = datetime.datetime.now()
-    print(f"starting request for huc {huc2} at {request_start_time}, for period {start_date} to {end_date}")
+    print(f"starting request for sites {sites} at {request_start_time}, "
+          f"for period {start_date} to {end_date}")
     r = requests.get(url)
     request_end_time = datetime.datetime.now()
     request_time = request_end_time - request_start_time
-    print(f"took {request_time} to get data for huc {huc2}")
+    print(f"took {request_time} to get data for huc {sites}")
     return r
 
 
@@ -68,7 +104,7 @@ def resample_reindex(df, start_date, end_date, time_scale):
 
     # get new index
     date_index = pd.date_range(start=start_date, end=end_date,
-                                  freq=time_scale)
+                               freq=time_scale)
     # make so the index goes from start to end regardless of actual data
     # presence
     df_reindexed = df_resamp.reindex(date_index)
@@ -115,8 +151,9 @@ def format_df(ts_df, site_code, start_date, end_date, time_scale,
     ts_df['dateTime'] = format_dates(ts_df['dateTime'])
     ts_df.set_index('dateTime', inplace=True)
 
-    # get rid of any points that were not approved
-    ts_df = delete_non_approved_data(ts_df)
+    if only_approved:
+        # get rid of any points that were not approved
+        ts_df = delete_non_approved_data(ts_df)
     # delete qualifiers column 
     del ts_df['qualifiers']
     # rename the column from 'value' to the site_code
@@ -145,33 +182,20 @@ def nwis_json_to_df(json_data, start_date, end_date, time_scale='H'):
         ts_data = ts['values'][0]['value']
         if ts_data:
             ts_df = pd.DataFrame(ts_data)
-            ts_df_formatted = format_df(ts_df, site_code, start_date, end_date, time_scale)
+            ts_df_formatted = format_df(ts_df, site_code, start_date, end_date,
+                                        time_scale)
             df_collection.append(ts_df_formatted)
     if df_collection:
-        return pd.concat(df_collection)
+        df_combined = pd.concat(df_collection, axis=1)
+        df_combined = df_combined.replace(-999999, np.nan)
+        return df_combined
     else:
         return None
 
 
 hucs = [f'{h:02}' for h in range(1, 19)]
-i = 0
-times = []
-data = []
-# for huc in hucs:
-        # xr = all_huc_df.to_xarray()
-        # xr.to_zarr(f'{huc}_streamflow')
-
-sites = get_sites_for_huc2('01')
-# zarr_file = "E:\\data\\streamflow_data\\zarr_huc1"
-# all_huc_df = get_all_streamflow_data_for_huc2(hucs[0], 2, "1990-01-07", "1990-01-08")
-# d_array = xr.DataArray(all_huc_df.values, coords=[('datetime', all_huc_df.index), ('site_code', all_huc_df.columns)])
-# ds = xr.Dataset({'streamflow': d_array})
-# ds.to_zarr(zarr_file)
-#
-# all_huc_df2 = get_all_streamflow_data_for_huc2(hucs[0], 2, "1990-01-09", "1990-01-11")
-# d_array2 = xr.DataArray(all_huc_df2.values, coords=[('datetime', all_huc_df2.index), ('site_code', all_huc_df2.columns)])
-# ds2 = xr.Dataset({'streamflow': d_array2})
-# ds2.to_zarr(zarr_file, mode='a', append_dim='datetime')
-# ds2 = xr.Dataset({'streamflow':all_huc_df2.values}, coords={'datetime': all_huc_df2.index,
-#                                                            'site_code':all_huc_df2.columns})
-# print ('jjk')
+if __name__ == '__main__':
+    get_all_streamflow_data_for_huc2('02',
+                                     "E:\\data\\streamflow_data\\"
+                                     "discharge_data_02",
+                                     num_sites_per_chunk=5)
