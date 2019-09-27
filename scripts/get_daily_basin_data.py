2afe147e7e184aa16b1a23250797bbc18ddb5b1b
Sadler <jsadler@usgs.gov>
2019-09-27 12:55:21 -0500

    daily data for all huc2's

--- scripts/get_daily_basin_data.py
+++ scripts/get_daily_basin_data.py
@@ -1,5 +1,13 @@
 from get_all_streamflow_data import get_all_streamflow_data_for_huc2
-get_all_streamflow_data_for_huc2('02', "E:\\data\\streamflow_data\\"
-                                 "discharge_data_02_daily",
-                                 num_sites_per_chunk=10, product='dv',
-                                 time_scale='D')
+import datetime
+
+hucs = [f'{h:02}' for h in range(1, 19)]
+for huc in hucs:
+    get_all_streamflow_data_for_huc2(huc, "E:\\data\\streamflow_data\\"
+                                     f"discharge_data_{huc}_daily",
+                                     num_sites_per_chunk=10, product='dv',
+                                     time_scale='D')
+    with open(f'../data/daily_discharge/indicator_{huc}', 'w') as f:
+        f.write(f'successfully pulled daily discharge data for {huc} \n')
+        f.write(str(datetime.datetime.now()))
+
