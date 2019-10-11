b57622af034c6ef4709a00f023f7a3a2bb15b699
Sadler <jsadler@usgs.gov>
2019-10-11 14:43:56 -0500

    added output format to get daily streamflow

--- scripts/get_daily_basin_data.py
+++ scripts/get_daily_basin_data.py
@@ -4,9 +4,9 @@ from utils import hucs
 
 for huc in hucs:
     get_all_streamflow_data_for_huc2(huc, "E:\\data\\streamflow_data\\"
-                                     f"discharge_data_{huc}_daily",
-                                     num_sites_per_chunk=10, product='dv',
-                                     time_scale='D')
+                                     f"discharge_data_{huc}_daily.csv",
+                                     num_sites_per_chunk=20, product='dv',
+                                     time_scale='D', output_format='csv')
     with open(f'../data/daily_discharge/indicator_{huc}.txt', 'w') as f:
         f.write(f'successfully pulled daily discharge data for {huc} \n')
         f.write(str(datetime.datetime.now()))
