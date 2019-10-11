2d46d6dbf283b53317e75a15587de2f5fc5acf21
Sadler <jsadler@usgs.gov>
2019-10-11 16:06:12 -0500

    removed product in get all daily

--- scripts/get_all_daily_streamflow_data.py
+++ scripts/get_all_daily_streamflow_data.py
@@ -5,8 +5,8 @@ from utils import hucs
 for huc in hucs:
     get_all_streamflow_data_for_huc2(huc, "E:\\data\\streamflow_data\\"
                                      f"discharge_data_{huc}_daily.csv",
-                                     num_sites_per_chunk=20, product='dv',
-                                     time_scale='D', output_format='csv')
+                                     num_sites_per_chunk=20, time_scale='D',
+                                     output_format='csv')
     with open(f'../data/daily_discharge/indicator_{huc}.txt', 'w') as f:
         f.write(f'successfully pulled daily discharge data for {huc} \n')
         f.write(str(datetime.datetime.now()))
