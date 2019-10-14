bf7b48fc45f4d4a66c0c90d5d6468eb1e71468e1
Sadler <jsadler@usgs.gov>
2019-10-14 16:52:12 -0500

    snakemake variables for daily discharge

--- scripts/get_all_daily_streamflow_data.py
+++ scripts/get_all_daily_streamflow_data.py
@@ -1,13 +1,21 @@
 from streamflow_data_retrival import get_all_streamflow_data_for_huc2
-import datetime
-from utils import hucs
+from utils import write_indicator_file
 
+hucs = snakemake.params.hucs
+out_file_names = snakemake.params.out_data_files
+indicator_file_name = snakemake.output
+# snakemake.input[0] is the 'iv'
+sites_file = snakemake.input[1]
+
+i = 0
 for huc in hucs:
-    get_all_streamflow_data_for_huc2(huc, "E:\\data\\streamflow_data\\"
-                                     f"discharge_data_{huc}_daily.csv",
+    get_all_streamflow_data_for_huc2(huc, out_file_names[i], sites_file,
                                      num_sites_per_chunk=20, time_scale='D',
                                      output_format='csv')
-    with open(f'../data/daily_discharge/indicator_{huc}.txt', 'w') as f:
-        f.write(f'successfully pulled daily discharge data for {huc} \n')
-        f.write(str(datetime.datetime.now()))
+
+    write_indicator_file(get_all_streamflow_data_for_huc2,
+                         file_name=indicator_file_name[i])
+    i += 1
+
+
 
