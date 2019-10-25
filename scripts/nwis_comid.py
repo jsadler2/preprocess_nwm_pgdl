4962ee1d27a304b2f0e95071a2a8907ec685e071
jsadler2 <jsadler@usgs.gov>
2019-10-25 16:46:56 -0500

    nwis comid table from nhd

--- scripts/nwis_comid.py
+++ scripts/nwis_comid.py
@@ -4,37 +4,30 @@ Get the nhd comid's that correspond to nwis site codes
 
 import pandas as pd
 
-from utils import get_sites_for_huc2, generate_nldi_url, \
-    json_from_nldi_request, get_nldi_data_huc2, write_indicator_file
 
+def get_comids_for_all_nwis_nhd(outfile, sites_file_name, nhd_file_name):
+    # read in all_nhd "gage" data and make sure the site code column is read in
+    # as a string
+    nhd_site_code_col_name = 'SOURCE_FEA'
+    comid_col_name = 'FLComID'
+    nhd_df = pd.read_csv(nhd_file_name, dtype={nhd_site_code_col_name:str,
+                                               comid_col_name:str})
 
-def comid_data_to_df(nwis_site, json_data):
-    comid = json_data['features'][0]['properties']['comid']
-    df = pd.DataFrame(data=[str(comid)], index=[str(nwis_site)],
-                      columns=['comid'])
-    df.index.name = 'nwis_site_code'
-    return df
+    # set the site code as the index so we can select by it using site codes
+    # from the nwis data
+    nhd_df.set_index(nhd_site_code_col_name, inplace=True)
 
+    nwis_sites_df = pd.read_csv(sites_file_name, dtype=str, header=None)
 
-def get_comid_for_one_nwis(nwis_site):
-    url = generate_nldi_url('nwis', nwis_site)
-    json_data = json_from_nldi_request(url)
-    df = comid_data_to_df(nwis_site, json_data)
-    return df
+    # select the nhd data that corresponds to the nwis sites
+    nhd_df_discharge = nhd_df.loc[nwis_sites_df[0]]
 
+    # rename the index
+    nhd_df_discharge.index.name = 'nwis_site_code'
 
-def get_comids_for_all_nwis(outfile, outfile_type, sites_file_name):
-    # Warning NLDI only has the "iv" NWIS sites
-    # get all sites numbers
-    sites = get_sites_for_huc2(sites_file_name)
-    print(outfile)
-    get_nldi_data_huc2(sites, outfile, get_comid_for_one_nwis,
-                       'nwis_site_code', outfile_type)
+    # get just the comid column, rename, and save
+    just_comids = nhd_df_discharge[comid_col_name]
+    just_comids.name = 'comid'
+    just_comids = just_comids[~just_comids.isna()]
+    just_comids.to_csv(outfile, index=True, header=True)
 
-
-if __name__ == "__main__":
-    out_file = snakemake.params.out_file
-    site_file = snakemake.input[0]
-    indicator_file = snakemake.output[0]
-    get_comids_for_all_nwis(out_file, 'csv', site_file)
-    write_indicator_file(get_comids_for_all_nwis, indicator_file)
